fa2fb43e90ccee19e5d8f099eaf1d763ce9d154a
Foggg Snail <you@example.com>
2020-12-15 13:33:39 +0700

    full

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const FooterContainer = styled.footer`
+  background-color: #0d0909;
+`;
+
+export const FooterWrap = styled.div`
+  padding: 16px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1300px;
+  margin: 0 auto;
+`;
+
+export const SocialMedia = styled.section`
+  max-width: 1300px;
+  width: 100%;
+`;
+
+export const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 16px auto 0 auto;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
+  transition: transform 0.15s;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+  transition: transform 0.15s;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+
+export const Copyright = styled.div`
+  color: #fff;
+  padding-bottom: 1rem;
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import { GiSpikedSnail } from "react-icons/gi";
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+  Copyright,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Pizza</SocialLogo>
+            <Copyright>
+              <GiSpikedSnail />F o g g g © 2 0 2 0
+            </Copyright>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="Twitter"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
